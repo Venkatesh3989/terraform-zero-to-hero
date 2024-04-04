2ccebb154f01d37df4a99fd465cb71e6cab47c31
Venkatesh3989 <138436125+Venkatesh3989@users.noreply.github.com>
2024-04-04 12:01:46 +0000

    valut update

--- Day-7/main.tf
+++ Day-7/main.tf
@@ -1,32 +1,31 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 provider "vault" {
-  address = "<>:8200"
+  address = "http://18.191.191.145:8200"
   skip_child_token = true
 
   auth_login {
     path = "auth/approle/login"
 
     parameters = {
-      role_id = "<>"
-      secret_id = "<>"
+      role_id = "032c5faa-186f-6fca-fb2b-d1e832f72efb"
+      secret_id = "47e8a32b-c25c-019f-25f3-1ef697078304"
     }
   }
 }
 
 data "vault_kv_secret_v2" "example" {
-  mount = "secret" // change it according to your mount
+  mount = "kv-1" // change it according to your mount
   name  = "test-secret" // change it according to your secret
 }
 
 resource "aws_instance" "my_instance" {
-  ami           = "ami-053b0d53c279acc90"
+  ami           = "ami-0b8b44ec9a8f90422"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "test"
-    Secret = data.vault_kv_secret_v2.example.data["foo"]
+    Secret = data.vault_kv_secret_v2.example.data["admin"]
   }
 }
